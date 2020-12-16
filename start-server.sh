5aa557581eebc81b50d30b74980070da088ac4fb
sahil555 <sahil555kureel@gmail.com>
2020-12-16 21:27:57 +0530

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd SampleApp; python3 manage.py createsuperuser --no-input)
 
-(cd SampleApp; gunicorn martor_demo.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
+(cd SampleApp; gunicorn SampleApp.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
